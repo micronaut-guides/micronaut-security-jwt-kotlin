93496bec04a684a0e3964990f9b4c0b30db64743
Sergio del Amo <sergio.delamo@softamo.com>
2020-06-22 13:26:02 +0200

    Updata to 2.0.0.RC1

--- complete/src/test/kotlin/example/micronaut/AppClient.kt
+++ complete/src/test/kotlin/example/micronaut/AppClient.kt
@@ -1,12 +1,18 @@
 package example.micronaut
 
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Header
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.*
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.security.authentication.UsernamePasswordCredentials
+import io.micronaut.security.token.jwt.render.BearerAccessRefreshToken
 
 @Client("/")
 interface AppClient {
+    @Post("/login")
+    fun login(@Body credentials : UsernamePasswordCredentials): BearerAccessRefreshToken
 
+
+    @Consumes(MediaType.TEXT_PLAIN)
     @Get("/")
     fun home(@Header authorization: String): String
 }
